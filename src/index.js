04c4bbc71aa668ba0e5ffdc9aa3c990739bbe7b6
Dmitry Muzychenko <nogrevonlight@gmail.com>
2020-09-29 01:49:29 +0300

    feat: add functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!Array.isArray(array) || !array.length) return 0;
+  array.sort((val1, val2) => {
+    if(val1 > val2) {
+      return 1;
+    }
+    else {
+      return -1;
+    }
+  });
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!Array.isArray(array) || !array.length) return 0;
+  array.sort((val1, val2) => {
+    if(val1 < val2) {
+      return 1;
+    }
+    else {
+      return -1;
+    }
+  });
+  return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!Array.isArray(array) || !array.length) return 0;
+  return array.reduce((acc, val) => acc + val)/array.length;
 }
